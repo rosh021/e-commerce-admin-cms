ef3c0c87a0141151e09fb56d69111c2e92df5f8c
rosh021 <roshanbhattarai021@gmail.com>
2022-06-26 10:18:02 +1000

    using nodemailer

---
+++ api/src/helper/emailHelper.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendMail = () => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_SMTP,
+    port: +process.env.MAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER, // generated ethereal user
+      pass: process.env.MAIL_PASS, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+};
